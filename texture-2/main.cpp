fe927f9950c426a881d33e30685063c7139c9402
edtorrez <etorrez2017@gmail.com>
2020-06-05 11:44:05 -0400

    seperate folder for textures

---
+++ texture-2/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Shader/ShaderProgram.h>
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+int main()
+{
+    
+}
