770f2a782d02a6b6ce1ae9b866a61a96ef832266
edtorrez <etorrez2017@gmail.com>
2020-06-08 19:51:22 -0400

    fixed resize issue

--- texture-1/main.cpp
+++ texture-1/main.cpp
@@ -124,7 +124,7 @@ int main()
 
 void resize_callback( GLFWwindow* window, int width, int height )
 {
-    glViewport( 0, 0, 600, 600 );
+    glViewport( 0, 0, width, height );
 }
 
 void key_callback( GLFWwindow* window, int key, int scancode, int action, int mods )
